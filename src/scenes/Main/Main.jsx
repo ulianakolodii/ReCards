60a43519804357c05bf695006541a73b5783efa8
Eugen Guriev <gurievcreative@gmail.com>
2022-09-09 22:41:26 +0300

    feat: working on navigation

--- src/scenes/Main/Main.jsx
+++ src/scenes/Main/Main.jsx
@@ -2,38 +2,66 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Header, Box } from "@primer/react";
 
-export const Main = ({ children }) => {
-  return (
-    <div>
-      <Header>
-        <Header.Item>
-          <Header.Link as={NavLink} to="/" fontSize={2}>
-            <Box
-              as="img"
-              src="../../../logo.png"
-              alt="Logo"
-              width={32}
-              height={32}
-            />
-          </Header.Link>
-        </Header.Item>
-        <Header.Item>
-          <Header.Link as={NavLink} to="/">
-            Візити
-          </Header.Link>
-        </Header.Item>
-        <Header.Item>
-          <Header.Link as={NavLink} to="/patients">
-            Пацієнти
-          </Header.Link>
-        </Header.Item>
-        <Header.Item>
-          <Header.Link as={NavLink} to="/doctors">
-            Лікарі
-          </Header.Link>
-        </Header.Item>
-      </Header>
-      <div>{children}</div>
-    </div>
-  );
-};
+export const Main = ({ children }) => (
+  <div>
+    <Header>
+      <Header.Item>
+        <Header.Link as={NavLink} to="/" fontSize={2}>
+          <Box
+            as="img"
+            src="../../../logo.png"
+            alt="Logo"
+            width={32}
+            height={32}
+          />
+        </Header.Link>
+      </Header.Item>
+      <Header.Item>
+        <Header.Link
+          as={NavLink}
+          to="/"
+          sx={{
+            "&.active": {
+              opacity: 0.5,
+              cursor: "default",
+              pointerEvents: "none",
+            },
+          }}
+        >
+          Візити
+        </Header.Link>
+      </Header.Item>
+      <Header.Item>
+        <Header.Link
+          as={NavLink}
+          to="/patients"
+          sx={{
+            "&.active": {
+              opacity: 0.5,
+              cursor: "default",
+              pointerEvents: "none",
+            },
+          }}
+        >
+          Пацієнти
+        </Header.Link>
+      </Header.Item>
+      <Header.Item>
+        <Header.Link
+          as={NavLink}
+          to="/doctors"
+          sx={{
+            "&.active": {
+              opacity: 0.5,
+              cursor: "default",
+              pointerEvents: "none",
+            },
+          }}
+        >
+          Лікарі
+        </Header.Link>
+      </Header.Item>
+    </Header>
+    <div>{children}</div>
+  </div>
+);
